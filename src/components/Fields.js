25ce6c0773d92891f46a61b225b1a883802c4238
Allen Stevens <allen.stevens@students.dominican.edu>
2024-05-06 12:24:43 -0700

    centered the home page and made it more responsive for smaller screens

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -77,7 +77,7 @@ const Fields = ({income, setIncome, rent, setRent, food,
         <Header />
 
         <form>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-20">
                 <div className="mb-4">
                     <label htmlFor="income" className="block">Income</label>
                         <input 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,22 @@ const Home = () => {
     const [name, setName] = useState('');
     return (
       <div className="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-600 to-blue-300">
-        <div className="p-6 rounded-md text-white max-w-full w-full sm:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto bg-gradient-to-r from-indigo-600 to-blue-300">
+        <div className="flex flex-col items-center justify-center p-6 rounded-md text-white max-w-full w-full sm:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto bg-gradient-to-r from-indigo-600 to-blue-300">
           <h1 className="text-4xl mb-4">Budget Buddy</h1>
           <p className="mb-4">
             Enter your name: 
             <input type="text" value={name} onChange={e => setName(e.target.value)} className="ml-2 p-1 rounded border border-white" />
           </p>
-          {name && <p className="mb-4">Hi {name}, Welcome to the expense tracker! We're glad you're here.</p>}
+          {name && <p className="mb-4">Hi {name}, Welcome to Budget Buddy! We're glad you're here.</p>}
           <nav>
             <p className="mb-2">Where would you like to go?</p>
             <Link to="/fields" className="block mb-2 text-2xl text-orange-500 hover:text-purple-900 font-bold">Expense Tracker</Link>
             <Link to="/spending" className="block mb-2 text-2xl text-orange-500 hover:text-purple-900 font-bold">Spending Record</Link>
             <Link to="/about" className="block mb-2 text-2xl text-orange-500 hover:text-purple-900 font-bold">About</Link> 
-            </nav>
+          </nav>
         </div>
       </div>
     );
-  }
+}
 
 export default Home;
